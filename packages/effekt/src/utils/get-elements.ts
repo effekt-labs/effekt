1807ae54738dbaf2c7377fd185816d3ded9c1c4b
Ivo Dolenc <ivodolenc@gmail.com>
2025-04-12 13:46:17 +0200

    refactor: add generics to get-elements util

--- packages/effekt/src/utils/get-elements.ts
+++ packages/effekt/src/utils/get-elements.ts
@@ -16,15 +16,17 @@ import type { AnimationTargets } from '@/animation/types'
  * getElements(document.querySelectorAll('.class')) // Returns [el1, el2, el3]
  * ```
  */
-export function getElements(targets: AnimationTargets): Element[] {
+export function getElements<T extends Element = Element>(
+  targets: AnimationTargets,
+): T[] {
   if (!isBrowser) return []
 
   const els = isString(targets) ? document.querySelectorAll(targets) : targets
 
-  if (isElement(els)) return [els]
+  if (isElement(els)) return [els] as T[]
   if (els) {
     const arr = !isArray(els) ? [...els] : els.filter((el) => isElement(el))
-    if (arr.length) return arr
+    if (arr.length) return arr as T[]
   }
 
   console.warn('Effekt: The specified elements were not found in the DOM.')
